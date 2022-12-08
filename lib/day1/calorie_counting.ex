f9bcff1e02c1fdb8946431a694a2ce8dac5cd849
Assisneto Damasceno <netodamasceno12@gmail.com>
2022-12-08 19:44:49 -0300

    Day 1 pt 2

--- lib/day1/calorie_counting.ex
+++ lib/day1/calorie_counting.ex
@@ -1,14 +1,32 @@
 defmodule Day1.CalorieCounting do
-  def get_elf_carrying_most_calories(input) do
-    input |> get_amount_each_elf_carries() |> Enum.max()
-  end
+  def get_elf_carrying_most_calories(input),
+    do:
+      input
+      |> get_amount_each_elf_carries()
+      |> Enum.max()
+
+  def get_top3_elf_carrying_most_calories(input),
+    do:
+      input
+      |> get_amount_each_elf_carries()
+      |> top3()
+
+  def top3(input),
+    do:
+      input
+      |> Enum.sort()
+      |> Enum.take(-3)
+      |> Enum.sum()
 
   defp get_amount_each_elf_carries(input),
-    do: input |> String.split("\n\n") |> Enum.map(&sum_elf_calories/1)
+    do:
+      input
+      |> String.split("\n\n")
+      |> Enum.map(&sum_elf_calories/1)
 
-  defp sum_elf_calories(each_elf_carries) do
-    each_elf_carries
-    |> String.split()
-    |> Enum.reduce(0, fn x, acc -> String.to_integer(x) + acc end)
-  end
+  defp sum_elf_calories(each_elf_carries),
+    do:
+      each_elf_carries
+      |> String.split()
+      |> Enum.reduce(0, fn x, acc -> String.to_integer(x) + acc end)
 end
