ad974f19ccbbc0bd886f4c56b34b7f9d080ca9d8
Assisneto Damasceno <netodamasceno12@gmail.com>
2022-12-12 23:07:02 -0300

    Day 2 pt 2

--- lib/day2/rock_paper_scissors.ex
+++ lib/day2/rock_paper_scissors.ex
@@ -3,27 +3,33 @@ defmodule Day2.RockPaperScissors do
     do:
       input
       |> String.split("\n")
-      |> calc(0)
+      |> calc_strategy_guide(0)
 
-  #   a = pedra
-  #   b = papel
-  #   c = tesoura
-  #   x = pedra
-  #   y = papel
-  #   z = tesoura
+  def strategy_guide2(input),
+    do:
+      input
+      |> String.split("\n")
+      |> calc_strategy_guide2(0)
 
-  def calc([head | tail], acc) do
-    score = head |> String.split(" ") |> pair()
-    calc(tail, acc + score)
+  defp calc_strategy_guide([head | tail], acc) do
+    score = head |> String.split(" ") |> strategy_guide_pair()
+    calc_strategy_guide(tail, acc + score)
   end
 
-  def calc([], acc), do: acc
+  defp calc_strategy_guide([], acc), do: acc
 
-  def pair(["A", "Y"]), do: 8
-  def pair(["B", "Z"]), do: 9
-  def pair(["C", "X"]), do: 7
+  defp calc_strategy_guide2([head | tail], acc) do
+    score = head |> String.split(" ") |> strategy_guide2_pair()
+    calc_strategy_guide2(tail, acc + score)
+  end
 
-  def pair([a, b]) do
+  defp calc_strategy_guide2([], acc), do: acc
+
+  defp strategy_guide_pair(["A", "Y"]), do: 8
+  defp strategy_guide_pair(["B", "Z"]), do: 9
+  defp strategy_guide_pair(["C", "X"]), do: 7
+
+  defp strategy_guide_pair([a, b]) do
     mapper = %{
       "A" => "X",
       "B" => "Y",
@@ -41,5 +47,37 @@ defmodule Day2.RockPaperScissors do
     else
       mapper1[b]
     end
+  end
+
+  defp strategy_guide2_pair([a, b]), do: mapper([a, b])
+
+  defp mapper([a, "X"]) do
+    mapperX = %{
+      "A" => 3,
+      "B" => 1,
+      "C" => 2
+    }
+
+    mapperX[a]
+  end
+
+  defp mapper([a, "Y"]) do
+    mapperY = %{
+      "A" => 4,
+      "B" => 5,
+      "C" => 6
+    }
+
+    mapperY[a]
+  end
+
+  defp mapper([a, "Z"]) do
+    mapperZ = %{
+      "A" => 8,
+      "B" => 9,
+      "C" => 7
+    }
+
+    mapperZ[a]
   end
 end
